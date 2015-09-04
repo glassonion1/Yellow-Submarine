448cf5cd57b3230586169d5a375df006e3e9f754
Taisuke Fujita <taisuke.fujita@g-pc-6292.intra.gree-office.net>
2015-09-04 17:36:00 +0900

    Fix bug of Sample2.

--- ES2.0/Sample2/Sample2/GLSurfaceView.m
+++ ES2.0/Sample2/Sample2/GLSurfaceView.m
@@ -8,9 +8,41 @@
 
 #import "GLSurfaceView.h"
 
+
+// シェーダーをコンパイルする関数
+GLuint compileShader(GLuint shaderType, const GLchar *source)
+{
+  // シェダーオブジェクト生成
+  GLuint shader = glCreateShader(shaderType);
+  // シェーダーオブジェクトとソースコードを結びつける
+  glShaderSource(shader, 1, &source, NULL);
+  // GLSLのコンパイル
+  glCompileShader(shader);
+  
+  // エラーチェック
+  GLint result;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+  // ログを取得
+  if (result == GL_FALSE) {
+    // サイズを取得
+    GLint log_length;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+    
+    // 文字列を取得
+    GLsizei max_length;
+    GLsizei length;
+    GLchar log[max_length];
+    glGetShaderInfoLog(shader, max_length, &length, log);
+    NSLog(@"%s", log);
+  }
+  assert(result == GL_TRUE);
+  return shader;
+}
+
+
 @implementation GLSurfaceView {
   EAGLContext *_context;
-  GLuint shaderProgram;
+  GLuint _shaderProgram;
 }
 
 + (Class)layerClass {
@@ -47,90 +79,69 @@
   glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
   glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, renderBuffer);
   
-  // 頂点シェダーオブジェクト生成
-  GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+  
   // 頂点シェーダーソースコード
-  const char *vertShaderSource = "attribute mediump vec4 pos;"
-                                 "void main() {"
-                                 "  gl_Position = pos;"
-                                 "}";
-  // シェーダーオブジェクトとソースコードを結びつける
-  glShaderSource(vertShader, 1, &vertShaderSource, NULL);
-  // GLSLのコンパイル
-  glCompileShader(vertShader);
+  const GLchar *vertShaderSource = "attribute mediump vec4 pos;"
+                                   "void main() {"
+                                   "  gl_Position = pos;"
+                                   "}";
+  // 頂点シェダーオブジェクト生成
+  GLuint vertShader = compileShader(GL_VERTEX_SHADER, vertShaderSource);
   
-  {
-    // エラーチェック
-    GLint result;
-    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &result);
-    
-    // ログを取得
-    if (result == GL_FALSE) {
-      // サイズを取得
-      GLint log_length;
-      glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &log_length);
-      
-      // 文字列を取得
-      GLsizei max_length;
-      GLsizei length;
-      GLchar log[max_length];
-      glGetShaderInfoLog(vertShader, max_length, &length, log);
-      NSLog(@"%s", log);
-    }
-  }
-  
-  
-  
-  // フラグメントシェダーオブジェクト生成
-  GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
   // フラグメントシェーダーソースコード
-  const char *fragShaderSource = "void main() {"
-                                 "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
-                                 "}";
-  // シェーダーオブジェクトとソースコードを結びつける
-  glShaderSource(fragShader, 1, &fragShaderSource, NULL);
-  // GLSLのコンパイル
-  glCompileShader(fragShader);
+  const GLchar *fragShaderSource = "void main() {"
+                                   "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
+                                   "}";
+  // フラグメントシェダーオブジェクト生成
+  GLuint fragShader = compileShader(GL_FRAGMENT_SHADER, fragShaderSource);
   
   // プログラムオブジェクトの生成
-  shaderProgram = glCreateProgram();
+  _shaderProgram = glCreateProgram();
   // 頂点シェーダーとプログラムオブジェクトを結びつける
-  glAttachShader(shaderProgram, vertShader);
+  glAttachShader(_shaderProgram, vertShader);
   // フラグメントシェーダーとプログラムオブジェクトを結びつける
-  glAttachShader(shaderProgram, fragShader);
+  glAttachShader(_shaderProgram, fragShader);
   // リンク
-  glLinkProgram(shaderProgram);
+  glLinkProgram(_shaderProgram);
+  
+  // シェーダーオブジェクトの解放
+  glDeleteShader(vertShader);
+  glDeleteShader(fragShader);
+  
   GLint linked;
-  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &linked);
+  glGetProgramiv(_shaderProgram, GL_LINK_STATUS, &linked);
   if (linked == GL_FALSE) {
     NSLog(@"%@", @"リンクエラー");
   }
   
   // シェーダーの利用を開始する
-  glUseProgram(shaderProgram);
+  glUseProgram(_shaderProgram);
+  
+  // 背景色の設定
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
 }
 
 - (void)layoutSubviews
 {
+  // ViewPortの設定
   glViewport(0, 0, self.frame.size.width, self.frame.size.height);
 }
 
 - (void)update:(NSTimer *)timer
 {
-  // カラーバッファのクリア
-  glClear(GL_COLOR_BUFFER_BIT);
   // 頂点シェーダーのpos変数の位置を取得する
-  GLint posLocation = glGetAttribLocation(shaderProgram, "pos");
-  // 頂点データアクセスを有効にする
+  GLint posLocation = glGetAttribLocation(_shaderProgram, "pos");
+  // pos変数へのアクセスを有効にする
   glEnableVertexAttribArray(posLocation);
   // 頂点データ
-  GLfloat vertex[] = {
-    0.0f, 0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f
+  const GLfloat vertex[] = {
+    0.0f, 0.5f,
+    -0.5f, -0.5f,
+    0.5f, -0.5f,
   };
   // 頂点データを作成する
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertex);
+  glVertexAttribPointer(posLocation, 2, GL_FLOAT, GL_FALSE, 0, vertex);
   // 描画する
   glDrawArrays(GL_TRIANGLES, 0, 3);
   
@@ -140,6 +151,14 @@
   glGetFloatv(GL_COLOR_CLEAR_VALUE, rgba);
   NSLog(@"%f %f %f %f", rgba[0], rgba[1], rgba[2], rgba[3]);
    */
+}
+
+- (void)dealloc
+{
+  // シェーダーの利用を終了する
+  glUseProgram(0);
+  // シェーダープログラムの解放
+  glDeleteProgram(_shaderProgram);
 }
 
 @end
